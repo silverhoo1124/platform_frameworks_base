10d81dd1a99e12e5642cb02280e56bb2d05b6975
donghaomin <dhmcharles@gmail.com>
2014-03-22 09:57:44 +0100

    [1/2] Status bar brightness control
    Add option to control brightness by sliding across status bar.
    Thanks to CyanogenMod for the original implementation.
    
    [1/2] 通知栏亮度控制
    
    添加在通知栏上划动以调整亮度的选项.
    感谢CyanogenMod, 此功能是由他们原创实现的.
    
    [2/2] in platform_packages_apps_Settings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2432,6 +2432,13 @@ public final class Settings {
         public static final String QUICK_SETTINGS_TILES = "quick_settings_tiles";
 
         /**
+         * Whether to control brightness from status bar
+         * 通知栏亮度控制选项
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
