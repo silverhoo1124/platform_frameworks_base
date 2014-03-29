4ea4937c019b79732eb5873888d343c8c13fcb4d
donghaomin <dhmcharles@gmail.com>
2014-03-29 18:55:24 +0100

    [1/2] User configurable safe headset volume
    * Add a user configurable option to
        enable/disable safe headset volume.
    * Add restore volumes after boot and when a headset is plugged.
    
    [1/2] 用户可配置的安全耳机音量
    
    * 添加一个用户可配置的选项
        来启用/禁用安全耳机音量.
    * 添加耳机插入时, 设备启动后恢复音量.
    
    [2/2] in platform_packages_apps_Settings
    
    Thanks to CyanogenMod for the original implementation.
    感谢CyanogenMod原创实现此功能.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2669,6 +2669,13 @@ public final class Settings {
         public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
 
         /**
+         * Whether to prevent loud volume levels when headset is first plugged in
+         * 定义是否在耳机首次插入时避免过高音量
+         * @hide
+         */
+        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
